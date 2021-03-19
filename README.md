f33a1550b9cd0135dc4eba7a13d7c44360d07a10
qnguy <qtn3@njit.edu>
2021-03-18 19:34:52 -0500

    Basic App Post

---
+++ README.md
@@ -0,0 +1,4 @@
+you need to do a composer install after cloning and you will need to do the npm run build command on the tutorial
+
+This is based on the following tutorial:
+https://www.itsolutionstuff.com/post/laravel-8-livewire-crud-with-jetstream-tailwind-cssexample.html
---
+++ database/migrations/2021_02_23_170033_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('body');
+            //create the relationship between a task and the user that created it
+            $table->integer('user_id')->unsigned()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
